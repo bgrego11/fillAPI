9de089c4d94d10305fde7cc32c4514aa8104ad67
bgrego11 <natureboy4271@gmail.com>
2021-03-12 19:55:59 -0500

    userprog routes

--- app/models/userprogress.model.js
+++ app/models/userprogress.model.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, Sequelize) => {
 
     const UserProgress = sequelize.define('userprogress', {
         user_id: {
-          type: Sequelize.INTEGER,
+          type: Sequelize.STRING,
         }
       }, {
         freezeTableName: true // Model tableName will be the same as the model name
