135861d26d61b75c3af3406b8e81d6114b9b68d9
bgrego11 <natureboy4271@gmail.com>
2021-05-19 17:25:15 -0400

    save

--- server.js
+++ server.js
@@ -31,8 +31,6 @@ require("./app/routes/story.routes")(app);
 require("./app/routes/section.routes")(app);
 require("./app/routes/userprogress.routes")(app);
 require("./app/routes/stripe.routes")(app);
-require("./app/routes/st_tag.routes")(app);
-require("./app/routes/carousel.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
