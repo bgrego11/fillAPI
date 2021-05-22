f5db19fa86afa3fb0e110f25fc23deeea10874e9
bgrego11 <natureboy4271@gmail.com>
2021-05-22 11:56:51 -0400

    cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-const server = require('http').Server(app);
 
 const app = express();
 
@@ -21,11 +20,11 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "./fillreact", "build")));
+app.use(express.static(path.join(__dirname, "/fillreact", "build")));
 app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './fillreact/build', 'index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/fillreact/build', 'index.html'))
 })
 
 require("./app/routes/series.routes")(app);
@@ -35,6 +34,6 @@ require("./app/routes/userprogress.routes")(app);
 require("./app/routes/stripe.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
